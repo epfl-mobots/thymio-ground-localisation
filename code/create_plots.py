c1e7ee4becea6dac6c1c83abc7a7f06d11ce0a17
Stéphane Magnenat <stephane@magnenat.net>
2015-09-09 13:47:26 +0200

    Improved plotting tool.

--- code/create_plots.py
+++ code/create_plots.py
@@ -8,33 +8,49 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-if __name__ == '__main__':
-	result_base_dir = '../result'
-	data_base_dir = '../data'
+result_base_dir = '../result'
+data_base_dir = '../data'
 
-	#mcl_params = [5, 10, 20, 50, 100, 150, 200, 400]
-	mcl_params = [10, 20, 50, 100, 150, 200, 400]
-	runs = ['random_1', 'random_2']
-	x_ticks = np.arange(0., 140.)
+def draw_plot(algo, runs, params, showDistNotAngle):
 
-	for mcl_param in mcl_params:
+	# show dist or angle diff?
+	if showDistNotAngle:
+		dataCol = 8
+		ylabel = 'distance to ground-truth position [cm]'
+	else:
+		dataCol = 9
+		ylabel = 'distance to ground-truth orientation [degrees]'
+
+	# load and average all given runs
+	x_ticks = np.arange(0., 100.)
+	for param in params:
 		y_average_values = np.zeros(shape=x_ticks.shape, dtype=float)
 		for run in runs:
-			data = np.loadtxt(os.path.join(result_base_dir, '{}_mcl_{}k'.format(run, mcl_param)))
+			data = np.loadtxt(os.path.join(result_base_dir, '{}_{}_{}'.format(run, algo, param)))
 			gt = np.loadtxt(os.path.join(data_base_dir, run, 'gt.txt'))
 			indices = data[:,0].astype(int)
 			xy = gt[indices,0:2]
 			deltas_xy = np.linalg.norm(np.diff(xy, axis=0), axis=1)
 			x_values = np.insert(np.cumsum(deltas_xy), 0, [0.]) * 100.
-			y_values = data[:,8]
+			y_values = np.abs(data[:,dataCol])
 			y_average_values += np.interp(x_ticks, x_values, y_values)
 
 		# plot
 		y_average_values /= len(runs)
-		plt.plot(x_ticks, y_average_values, label=str(mcl_param))
+		plt.plot(x_ticks, y_average_values, label=str(param))
 		plt.xlabel('path length')
-		plt.ylabel('distance to ground-truth position')
+		plt.ylabel(ylabel)
 
 	# add legend and show plot
 	plt.legend()
 	plt.show()
+
+if __name__ == '__main__':
+	# draw plots
+	draw_plot('mcl', ['random_1'], ['50k', '100k', '200k', '400k'], True)
+	draw_plot('mcl', ['random_2'], ['50k', '100k', '200k', '400k'], True)
+	draw_plot('mcl', ['random_1', 'random_2'], ['50k', '100k', '200k', '400k'], True)
+	draw_plot('mcl', ['random_1', 'random_2'], ['50k', '100k', '200k', '400k'], False)
+	draw_plot('ml', ['random_1', 'random_2'], [18, 36, 54, 72], True)
+	draw_plot('ml', ['random_1', 'random_2'], [18, 36, 54, 72], False)
+
