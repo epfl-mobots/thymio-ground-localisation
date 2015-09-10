0113d04cc43a2e93c72e04d1b602acae2fb3131f
Stéphane Magnenat <stephane@magnenat.net>
2015-09-10 13:02:29 +0200

    Corrected output file names.

--- code/generate_multiple_mcl.py
+++ code/generate_multiple_mcl.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
 	for i in range(0,100):
 		for run in runs:
 			for mcl_param in mcl_params:
-				command = './test.py --eval_data ../data/{} --mcl_particles_count {} --performance_log ../result/multiple_mcl/{}_mcl_{}k_{}'.format(run, mcl_param*1000, run, mcl_param, i)
+				command = './test.py --eval_data ../data/{} --mcl_particles_count {} --performance_log ../result/multiple_mcl/{}_{}_mcl_{}k'.format(run, mcl_param*1000, run, i, mcl_param)
 				print 'Executing:', command
 				print 'Total percentage done: ', i, '%'
 				os.system(command)
