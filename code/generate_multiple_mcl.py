452114d460613861f34068fdc869d74d3aeb8e55
Stéphane Magnenat <stephane@magnenat.net>
2015-09-10 13:30:45 +0200

    Added option to generate results for random_long.

--- code/generate_multiple_mcl.py
+++ code/generate_multiple_mcl.py
@@ -11,12 +11,17 @@ if __name__ == '__main__':
 		os.mkdir('../result/multiple_mcl')
 	except OSError as e:
 		pass
-	runs = ['random_1', 'random_2']
+	#runs = ['random_1', 'random_2']
+	runs = ['random_long']
 	mcl_params = [50, 100, 200, 400]
-	for i in range(0,100):
+	for i in range(0,10):
+		print 'Total percentage done:', i*10, '%'
 		for run in runs:
 			for mcl_param in mcl_params:
-				command = './test.py --eval_data ../data/{} --mcl_particles_count {} --performance_log ../result/multiple_mcl/{}_{}_mcl_{}k'.format(run, mcl_param*1000, run, i, mcl_param)
+				if run == 'random_long':
+					command = './test.py --eval_data ../data/{} --mcl_particles_count {} --prob_uniform 0.1 --duration 700 --performance_log ../result/multiple_mcl/{}_{}_mcl_{}k'.format(run, mcl_param*1000, run, i, mcl_param)
+				else:
+					command = './test.py --eval_data ../data/{} --mcl_particles_count {} --performance_log ../result/multiple_mcl/{}_{}_mcl_{}k'.format(run, mcl_param*1000, run, i, mcl_param)
 				print 'Executing:', command
-				print 'Total percentage done: ', i, '%'
 				os.system(command)
+
